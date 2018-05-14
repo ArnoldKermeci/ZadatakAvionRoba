7f0dd8e412d0f2b75276bd33b67ba7624dd64ce3
ArnoldKermeci <k.arnold111@gmail.com>
2018-05-14 20:32:51 +0200

    Uradjena zadatak kreiranja tabela

--- src/main/java/zadaci/Zadaci1KreiranjeTabela.java
+++ src/main/java/zadaci/Zadaci1KreiranjeTabela.java
@@ -2,11 +2,16 @@ package zadaci;
 
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import model.Avion;
+import model.Roba;
 
 import java.io.IOException;
 import java.sql.SQLException;
 
 public class Zadaci1KreiranjeTabela {
+
+
     public static void main(String[] args) {
         ConnectionSource connectionSource = null;
         try {
@@ -22,6 +27,26 @@ public class Zadaci1KreiranjeTabela {
                     e.printStackTrace();
                 }
             }
+        }
+        try {
+            TableUtils.clearTable(connectionSource, Roba.class);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            TableUtils.clearTable(connectionSource, Avion.class);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            TableUtils.createTable(connectionSource, Roba.class);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            TableUtils.createTable(connectionSource, Avion.class);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
